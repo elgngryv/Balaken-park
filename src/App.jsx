5f6476d871e472906b86857e98b234044f9d45a4
Elgun Garayev <elgunqarayev030@gmail.com>
2026-07-27 02:13:54 +0400

    style: update hero section headline to reflect residential complex branding

--- src/App.jsx
+++ src/App.jsx
@@ -414,7 +414,7 @@ export default function App() {
               <br />
               <em>unudulmaz</em>
               <br />
-              əyləncə dünyası
+              yaşayış kompleksi
             </h1>
           </div>
 
